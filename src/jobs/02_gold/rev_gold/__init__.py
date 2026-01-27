db8fe9bbdb0d2d594e34f28df68da2bc5e82c2be
Michael Cruz <sv.mscruz@gmail.com>
2026-01-27 16:58:10 -0300

    fix: retornando a sequência inicial, rev_gold não será utilizada, seguindo seq conforme mensagem de Gustavo Lenin

--- src/jobs/02_gold/rev_gold/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-rev_gold: Roadmap alternativo de ABTs (Atraso→Recarga→Cadastro→Telco→Scores)
-
-Conforme proposta de Fernando Parahyba (Claro) na reunião de 07/01/2026.
-"""
--- src/jobs/02_gold/rev_gold/validators/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Validators para rev_gold ABTs
-"""
